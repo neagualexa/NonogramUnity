32df1918c293bab3a8d228f3d3ba473b19fa8b96
neagualexa <ngu.alexandra@gmail.com>
2024-05-07 22:00:55 +0100

    FEAT: new tutorial level introduced with UI Hints to use the game

---
+++ Assets/TutorialLevelHints.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class TutorialLevelHints : MonoBehaviour {
+
+    private ButtonAnimations buttonAnimations;
+    private TMP_Text hint_text;
+    private Animator arrowUIHint_animator;
+    private Animator arrowGridInfo_animator;
+    private Animator arrowColumn_animator;
+    private Animator arrowRow_animator;
+    private Animator arrowMeaning_animator;
+    private Animator arrowCheck_animator;
+    private Animator arrowHint_animator;
+
+    private List<Animator> animation_list;
+    private int UIHintIndex = 0;
+
+    private string[] UI_Hints = {
+        "Welcome to the tutorial level! You will have to solve the Nonogram puzzle and guess the meaning of the image that it represents. \n Press Next to see the next Tutorial Introduction Hint.",
+        "On the left is the grid where you will be solving the puzzles. Below you can find information regarding your latest clicked cell, the grid size (rows x columns), and remaining time to complete the level.",
+        "Here you can find the clues for each row and their respective index number. \nThe clues show the number of consecutive filled squares. If there are multiple numbers, there is at least one white cell between the groups of filled squares.",
+        "Here you can find the clues for each column and their respective index number. \nThe same rules apply as for the row clues.",
+        "This is where you can put your guess for the meaning of the image that the Nonogram puzzle represents. Try to guess the meaning after you have completed the puzzle on the left. \nYou can check your guess by pressing the check button or pressing <Enter> Key.",
+        "Here you can check if your current progress of the puzzle is correct. If you think you have completed the puzzle, press the check button to see if your solution is correct.",
+        "Lastly, here you can request for a hint. \nI highly recommend to press the hint button if you need help understanding the game rules or getting unstuck."
+    };
+
+    void Awake() {
+        buttonAnimations = GetComponent<ButtonAnimations>();
+        hint_text = GameObject.Find("HintText").GetComponent<TMP_Text>();
+        arrowUIHint_animator = GameObject.Find("ArrowUIHint").GetComponent<Animator>();
+        arrowGridInfo_animator = GameObject.Find("ArrowGridInfo").GetComponent<Animator>();
+        arrowColumn_animator = GameObject.Find("ArrowColumn").GetComponent<Animator>();
+        arrowRow_animator = GameObject.Find("ArrowRow").GetComponent<Animator>();
+        arrowMeaning_animator = GameObject.Find("ArrowMeaning").GetComponent<Animator>();
+        arrowCheck_animator = GameObject.Find("ArrowCheck").GetComponent<Animator>();
+        arrowHint_animator = GameObject.Find("ArrowHint").GetComponent<Animator>();
+
+        animation_list = new List<Animator> {
+            arrowUIHint_animator,
+            arrowGridInfo_animator,
+            arrowRow_animator,
+            arrowColumn_animator,
+            arrowMeaning_animator,
+            arrowCheck_animator,
+            arrowHint_animator
+        };
+
+        animation_list[0].SetTrigger("ArrowPopUp");
+    }
+
+    public void ShowUIHints() {
+        UIHintIndex = 0;
+        // open Hint popup panel with a welcome message
+        buttonAnimations.SetHintSectionVisible();
+
+        NextHint();
+    }
+
+    public void CloseHintPanel() {
+        GameObject.Find("HintPanel").SetActive(false);
+    }
+
+    public void NextHint() {
+        // increment the hint index
+        if ( UIHintIndex >= UI_Hints.Length ) {
+            // close the hint panel
+            hint_text.text = "You have reached the end of the tutorial level hints. Good luck on completing the first level!";
+        }
+        else if ( UIHintIndex == 0 ) {
+            hint_text.text = UI_Hints[UIHintIndex];
+            UIHintIndex += 1;
+        }
+        else
+        {
+            animation_list[UIHintIndex].SetTrigger("ArrowPopUp");
+            hint_text.text = UI_Hints[UIHintIndex];
+            UIHintIndex += 1;
+        }
+    }   
+
+    public void BackHint() {
+        // decrement the hint index
+        if ( UIHintIndex <= 0 ) {
+            UIHintIndex = 0;
+            hint_text.text = UI_Hints[UIHintIndex];
+        }
+        else 
+        {
+            UIHintIndex -= 1;
+            animation_list[UIHintIndex].SetTrigger("ArrowPopUp");
+            hint_text.text = UI_Hints[UIHintIndex];
+        }
+        
+    }
+
+}
